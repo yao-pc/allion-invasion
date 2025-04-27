4e1702091cc500f6dea2e838ba66baa26b9b7ac7
yao-pc <1573279370@qq.com>
2025-04-27 21:40:05 +0800

    allien

---
+++ alien_invatin.py
@@ -0,0 +1,41 @@
+import sys
+import pygame
+from setting import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """管理游戏资源和行为的类"""
+
+    def __init__(self):
+        """初始化游戏并创建游戏资源"""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+        # 设置背景色
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60)
+
+
+if __name__ == '__main__':
+    # 创建游戏实例并运行游戏
+    ai = AlienInvasion()
+    ai.run_game()
