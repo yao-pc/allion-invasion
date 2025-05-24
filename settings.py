0e660ba9f9540e4bb82b700464bff0d0f9b37afa
yao-pc <1573279370@qq.com>
2025-05-24 11:23:04 +0800

    完结

--- settings.py
+++ settings.py
@@ -9,17 +9,40 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # 飞船的设置
-        self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # 子弹设置
-        self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
         # 外星人设置
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
-        # fleet_direction 为 1 表示向右移动，为-1 表示向左移动
+
+        # 以什么速度加快游戏的节奏
+        self.speedup_scale = 1.1
+        # 外星人分数的提高速度
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行而变化的设置"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.5
+        self.alien_speed = 1.0
+        # fleet_direction 为 1 表示向右，为-1 表示向左
         self.fleet_direction = 1
+
+        # 记分设置
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """提高速度设置的值和外星人分数"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
